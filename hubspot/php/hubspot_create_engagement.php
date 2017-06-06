9ed6ed1d838fb4b2dfa8fdc0616a73da19ffeb50
Philippe Sultan <phsultan@apidaze.io>
2017-06-06 16:31:07 +0200

    Fix indentation.

--- hubspot/php/hubspot_create_engagement.php
+++ hubspot/php/hubspot_create_engagement.php
@@ -32,14 +32,14 @@ if (!isset($result['hapikey'])){
 $hapikey = $result['hapikey'];
 
 if($hapikey) {
-if($obj->event === "outgoing_call_ended" || $obj->event === "new_outgoing_call") {  
-  $phone = $obj->destination_number;
-} else  {
-  $phone = $obj->caller_id_number;
-}
-$logs .= 'PHONE: ' . $phone + ' hapikey ' + $hapikey;
+  if($obj->event === "outgoing_call_ended" || $obj->event === "new_outgoing_call") {  
+    $phone = $obj->destination_number;
+  } else  {
+    $phone = $obj->caller_id_number;
+  }
+  $logs .= 'PHONE: ' . $phone + ' hapikey ' + $hapikey;
   $contact = getHubspotContact($phone, $hapikey);
-  
+
   if($contact === null) {
     $logs .= "Cannot find HubSpot contact, returning.\n";
     $success = false;
@@ -67,134 +67,134 @@ $logs = "";
 
 $engagement = array(
   "engagement" => array(
-      "active" => true,
-      "ownerId"=> intval($contact->properties->hubspot_owner_id->value),
-      "type" => "CALL",
-      "timestamp" =>  time()*1000
-   ),
-   "associations" => array (
-      "contactIds"=> [
-         $contact->vid
-      ],
-      "companyIds"=> [],
-      "dealIds"=> [],
-      "ownerIds"=> [intval($contact->properties->hubspot_owner_id->value)]
-   ),
-   "attachments"=> [],
-   "metadata" => []
+    "active" => true,
+    "ownerId"=> intval($contact->properties->hubspot_owner_id->value),
+    "type" => "CALL",
+    "timestamp" =>  time()*1000
+  ),
+  "associations" => array (
+    "contactIds"=> [
+      $contact->vid
+    ],
+    "companyIds"=> [],
+    "dealIds"=> [],
+    "ownerIds"=> [intval($contact->properties->hubspot_owner_id->value)]
+  ),
+  "attachments"=> [],
+  "metadata" => []
 );
 
 
 
 switch($obj->event){
 
-  case "new_incoming_call":
-    $engagement["metadata"]["body"] = "Incoming call started <br />";
-    $engagement["metadata"]["body"] .= " From : +" . $obj->caller_id_number . $contact_name;
-    $engagement["metadata"]["body"] .= "<br /> To: +" . $obj->destination_number;
-    $engagement["metadata"]["toNumber"] = $obj->destination_number;
-    $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
-    $engagement["metadata"]["status"] = 'RINGING';
-    $engagement["metadata"]["externalId"] = '';
-    $engagement["metadata"]["durationMilliseconds"] = 0;
-    $engagement["metadata"]["externalAccountId"] = '';
-    $engagement["metadata"]["recordingUrl"] = '';
+case "new_incoming_call":
+  $engagement["metadata"]["body"] = "Incoming call started <br />";
+  $engagement["metadata"]["body"] .= " From : +" . $obj->caller_id_number . $contact_name;
+  $engagement["metadata"]["body"] .= "<br /> To: +" . $obj->destination_number;
+  $engagement["metadata"]["toNumber"] = $obj->destination_number;
+  $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
+  $engagement["metadata"]["status"] = 'RINGING';
+  $engagement["metadata"]["externalId"] = '';
+  $engagement["metadata"]["durationMilliseconds"] = 0;
+  $engagement["metadata"]["externalAccountId"] = '';
+  $engagement["metadata"]["recordingUrl"] = '';
 
   break;
 
-  case "incoming_call_answered":
-    $engagement["metadata"]["body"] = $obj->detailed_status . " <br />";
-    $engagement["metadata"]["body"] .= "From : +" . $obj->caller_id_number . $contact_name;
-    $engagement["metadata"]["body"] .= "<br /> To : +" . $obj->destination_number;
-    $engagement["metadata"]["toNumber"] = $obj->destination_number;
-    $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
-    $engagement["metadata"]["status"] = 'CONNECTING';
-    $engagement["metadata"]["externalId"] = '';
-    $engagement["metadata"]["durationMilliseconds"] = 0;
-    $engagement["metadata"]["externalAccountId"] = '';
-    $engagement["metadata"]["recordingUrl"] = '';
+case "incoming_call_answered":
+  $engagement["metadata"]["body"] = $obj->detailed_status . " <br />";
+  $engagement["metadata"]["body"] .= "From : +" . $obj->caller_id_number . $contact_name;
+  $engagement["metadata"]["body"] .= "<br /> To : +" . $obj->destination_number;
+  $engagement["metadata"]["toNumber"] = $obj->destination_number;
+  $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
+  $engagement["metadata"]["status"] = 'CONNECTING';
+  $engagement["metadata"]["externalId"] = '';
+  $engagement["metadata"]["durationMilliseconds"] = 0;
+  $engagement["metadata"]["externalAccountId"] = '';
+  $engagement["metadata"]["recordingUrl"] = '';
   break;
 
-  case "incoming_call_ended_and_missed":
-    $engagement["metadata"]["body"] = "Missed call <br />";
-    $engagement["metadata"]["body"] .= "From : +" . $obj->caller_id_number . $contact_name;
-    $engagement["metadata"]["body"] .= "<br /> To : +" . $obj->destination_number;
-    $engagement["metadata"]["toNumber"] = $obj->destination_number;
-    $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
-    $engagement["metadata"]["status"] = 'NO_ANSWER';
-    $engagement["metadata"]["externalId"] = '';
-    $engagement["metadata"]["durationMilliseconds"] = 0;
-    $engagement["metadata"]["externalAccountId"] = '';
-    $engagement["metadata"]["recordingUrl"] = '';
+case "incoming_call_ended_and_missed":
+  $engagement["metadata"]["body"] = "Missed call <br />";
+  $engagement["metadata"]["body"] .= "From : +" . $obj->caller_id_number . $contact_name;
+  $engagement["metadata"]["body"] .= "<br /> To : +" . $obj->destination_number;
+  $engagement["metadata"]["toNumber"] = $obj->destination_number;
+  $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
+  $engagement["metadata"]["status"] = 'NO_ANSWER';
+  $engagement["metadata"]["externalId"] = '';
+  $engagement["metadata"]["durationMilliseconds"] = 0;
+  $engagement["metadata"]["externalAccountId"] = '';
+  $engagement["metadata"]["recordingUrl"] = '';
 
   break;
 
-  case "incoming_call_ended_and_answered":
+case "incoming_call_ended_and_answered":
   $engagement["metadata"]["body"] =  $obj->detailed_status . " (duration : " . duration($obj->duration) . ") <br />";
 
-    $engagement["metadata"]["toNumber"] = $obj->destination_number;
-    $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
-    $engagement["metadata"]["status"] = 'COMPLETED';
-    $engagement["metadata"]["externalId"] = '';
-    $engagement["metadata"]["durationMilliseconds"] = $obj->duration*1000;
-    $engagement["metadata"]["externalAccountId"] = '';
-    $engagement["metadata"]["recordingUrl"] = '';
-    $engagement["metadata"]["body"] .= "From : +" . $obj->caller_id_number . $contact_name;
+  $engagement["metadata"]["toNumber"] = $obj->destination_number;
+  $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
+  $engagement["metadata"]["status"] = 'COMPLETED';
+  $engagement["metadata"]["externalId"] = '';
+  $engagement["metadata"]["durationMilliseconds"] = $obj->duration*1000;
+  $engagement["metadata"]["externalAccountId"] = '';
+  $engagement["metadata"]["recordingUrl"] = '';
+  $engagement["metadata"]["body"] .= "From : +" . $obj->caller_id_number . $contact_name;
 
-    $engagement["metadata"]["body"] .= "<br /> To : +" . $obj->destination_number;
-    if (property_exists($obj, 'recorded') && $obj->recorded == "true"){
-      $engagement["metadata"]["recordingUrl"] = $obj->recording_url;
-    }
-
-  break;
-
-  case "new_outgoing_call":
-    $engagement["metadata"]["body"] = "Outgoing call has just started";
-    $engagement["metadata"]["body"] .= "<br /> From : +" . $obj->caller_id_number . "<br />";
-    $engagement["metadata"]["body"] .= " To : +" . $obj->destination_number . $contact_name;
-    $engagement["metadata"]["toNumber"] = $obj->destination_number;
-    $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
-    $engagement["metadata"]["status"] = 'RINGING';
-    $engagement["metadata"]["externalId"] = '';
-    $engagement["metadata"]["durationMilliseconds"] = 0;
-    $engagement["metadata"]["externalAccountId"] = '';
-    $engagement["metadata"]["recordingUrl"] = '';
+  $engagement["metadata"]["body"] .= "<br /> To : +" . $obj->destination_number;
+  if (property_exists($obj, 'recorded') && $obj->recorded == "true"){
+    $engagement["metadata"]["recordingUrl"] = $obj->recording_url;
+  }
 
   break;
 
-  case "outgoing_call_ended":
-    $engagement["metadata"]["body"] = "Outgoing call ended (duration : " . duration($obj->duration) . ") <br />";
-    $engagement["metadata"]["body"] .= " From : +" . $obj->caller_id_number;
-    if (property_exists($obj, 'caller_id_name')) {
-      $engagement["metadata"]["body"] .= "  (" . $obj->caller_id_name . ")";
-    } 
-    $engagement["metadata"]["body"] .=  "<br /> To : +" . $obj->destination_number . $contact_name;
-    $engagement["metadata"]["toNumber"] = $obj->destination_number;
-    $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
-    $engagement["metadata"]["status"] = 'COMPLETED';
-    $engagement["metadata"]["externalId"] = '';
-    $engagement["metadata"]["durationMilliseconds"] = $obj->duration * 1000;
-    $engagement["metadata"]["externalAccountId"] = '';
-    $engagement["metadata"]["recordingUrl"] = '';
-
-    if (property_exists($obj, 'recorded') && $obj->recorded == "true"){
-      $engagement["metadata"]["recordingUrl"] = $obj->recording_url;
-    }
+case "new_outgoing_call":
+  $engagement["metadata"]["body"] = "Outgoing call has just started";
+  $engagement["metadata"]["body"] .= "<br /> From : +" . $obj->caller_id_number . "<br />";
+  $engagement["metadata"]["body"] .= " To : +" . $obj->destination_number . $contact_name;
+  $engagement["metadata"]["toNumber"] = $obj->destination_number;
+  $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
+  $engagement["metadata"]["status"] = 'RINGING';
+  $engagement["metadata"]["externalId"] = '';
+  $engagement["metadata"]["durationMilliseconds"] = 0;
+  $engagement["metadata"]["externalAccountId"] = '';
+  $engagement["metadata"]["recordingUrl"] = '';
 
   break;
 
-  case "incoming_call_ended_and_voicemail_left":
-    $engagement["metadata"]["body"] = "Received Voicemail (duration: " . duration($obj->voicemail_duration) . ")<br />";
-    $engagement["metadata"]["toNumber"] = $obj->destination_number;
-    $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
-    $engagement["metadata"]["status"] = 'COMPLETED';
-    $engagement["metadata"]["externalId"] = '';
-    $engagement["metadata"]["durationMilliseconds"] = $obj->voicemail_duration * 1000;
-    $engagement["metadata"]["externalAccountId"] = '';
-    $engagement["metadata"]["recordingUrl"] = $obj->voicemail_url;
-    $engagement["metadata"]["body"] .= "From : +" . $obj->caller_id_number . $contact_name . "<br />";
-    $engagement["metadata"]["body"] .= "To : +" . $obj->destination_number . "<br />";
-    $engagement["metadata"]["body"] .= "Transcription : " . $obj->voicemail_transcription;
+case "outgoing_call_ended":
+  $engagement["metadata"]["body"] = "Outgoing call ended (duration : " . duration($obj->duration) . ") <br />";
+  $engagement["metadata"]["body"] .= " From : +" . $obj->caller_id_number;
+  if (property_exists($obj, 'caller_id_name')) {
+    $engagement["metadata"]["body"] .= "  (" . $obj->caller_id_name . ")";
+  } 
+  $engagement["metadata"]["body"] .=  "<br /> To : +" . $obj->destination_number . $contact_name;
+  $engagement["metadata"]["toNumber"] = $obj->destination_number;
+  $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
+  $engagement["metadata"]["status"] = 'COMPLETED';
+  $engagement["metadata"]["externalId"] = '';
+  $engagement["metadata"]["durationMilliseconds"] = $obj->duration * 1000;
+  $engagement["metadata"]["externalAccountId"] = '';
+  $engagement["metadata"]["recordingUrl"] = '';
+
+  if (property_exists($obj, 'recorded') && $obj->recorded == "true"){
+    $engagement["metadata"]["recordingUrl"] = $obj->recording_url;
+  }
+
+  break;
+
+case "incoming_call_ended_and_voicemail_left":
+  $engagement["metadata"]["body"] = "Received Voicemail (duration: " . duration($obj->voicemail_duration) . ")<br />";
+  $engagement["metadata"]["toNumber"] = $obj->destination_number;
+  $engagement["metadata"]["fromNumber"] = $obj->caller_id_number;
+  $engagement["metadata"]["status"] = 'COMPLETED';
+  $engagement["metadata"]["externalId"] = '';
+  $engagement["metadata"]["durationMilliseconds"] = $obj->voicemail_duration * 1000;
+  $engagement["metadata"]["externalAccountId"] = '';
+  $engagement["metadata"]["recordingUrl"] = $obj->voicemail_url;
+  $engagement["metadata"]["body"] .= "From : +" . $obj->caller_id_number . $contact_name . "<br />";
+  $engagement["metadata"]["body"] .= "To : +" . $obj->destination_number . "<br />";
+  $engagement["metadata"]["body"] .= "Transcription : " . $obj->voicemail_transcription;
 
   break;
 
@@ -204,17 +204,17 @@ switch($obj->event){
 $url = "https://api.hubapi.com/engagements/v1/engagements?hapikey=" . $hapikey;
 $engagement_string = json_encode($engagement);
 $fplogs = fopen('/tmp/hubspot_create_engagement.txt', 'a+');
-  fwrite($fplogs, $engagement_string);
-  fclose($fplogs);
+fwrite($fplogs, $engagement_string);
+fclose($fplogs);
 //$engagements = json_encode($engagements);
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $engagement_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-'Accept: application/json'
+curl_setopt($ch, CURLOPT_POSTFIELDS, $engagement_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  'Content-Type: application/json',
+  'Accept: application/json'
 )
 );
 $output = curl_exec($ch);
@@ -301,7 +301,7 @@ function getHubspotContact($phone, $hapikey)
   $info = curl_getinfo($ch);
   curl_close($ch);
 
-$result = json_decode($output);
+  $result = json_decode($output);
   if(isset($result->total) && $result->total > 0) {
     return  $result->contacts[0];
   } else {
