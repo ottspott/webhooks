ee0617843dae0def1c2205a44cdd471d2d5e6260
Alexis Murer <alexis@ottspott.co>
2018-10-26 12:49:23 +0200

    zoho php webhook

---
+++ zoho/php/create_activity.php
@@ -0,0 +1,153 @@
+<?php
+
+$date = date('d-m-Y G:i:s');
+
+$success = true;
+$response['ok'] = true;
+
+$json = file_get_contents('php://input');
+$obj = json_decode($json);
+
+$logs .= "\n[". $date . "] JSON RECEIVED FROM OTTSPOTT :\n";
+$logs .= $json;
+$logs .= "\n";
+
+$fplogs = fopen('/tmp/zoho_create_activity.txt', 'a+');
+fwrite($fplogs, $logs);
+fclose($fplogs);
+
+$zoho_credentials = new \stdClass();
+$zoho_credentials->accessToken = $obj->zoho_oauth_token;
+$zoho_credentials->domain = $obj->zoho_domain;
+
+$logs = "";
+
+switch($obj->event){
+  case "new_incoming_call":
+    $url = "https://www.zohoapis.com/crm/v2/phonebridge/callreceived";
+    $parameters = array(
+      "callrefid" => $obj->call_uuid,
+      "fromnumber" => $obj->caller_id_number,
+      "tonumber" => $obj->destination_number,
+    );
+  break;
+  case "incoming_call_answered":
+    $url = "https://www.zohoapis.com/crm/v2/phonebridge/callanswered";
+    $parameters = array(
+      "callrefid" => $obj->call_uuid,
+      "fromnumber" => $obj->caller_id_number,
+      "tonumber" => $obj->destination_number,
+    );
+  break;
+  case "incoming_call_ended_and_missed":
+    $url = "https://www.zohoapis.com/crm/v2/phonebridge/callmissed";
+    $parameters = array(
+      "callrefid" => $obj->call_uuid,
+      "fromnumber" => $obj->caller_id_number,
+      "tonumber" => $obj->destination_number,
+      "callmissedtime" => prettyDate($obj->closed_at),
+    );
+  break;
+  case "incoming_call_ended_and_answered":
+    $url = "https://www.zohoapis.com/crm/v2/phonebridge/callhungup";
+    $parameters = array(
+      "callrefid" => $obj->call_uuid,
+      "fromnumber" => $obj->caller_id_number,
+      "tonumber" => $obj->destination_number,
+      "callstarttime" => prettyDate($obj->answered_at),
+      "direction" => "inbound",
+      "duration" => $obj->duration,
+    );
+  break;
+  case "new_outgoing_call":
+    $custstatus = "inprogress";
+    if (property_exists($obj, 'callstatus') && $obj->callstatus == "outcallanswered")
+      $custstatus = "success";
+    $url = "https://www.zohoapis.com/crm/v2/phonebridge/calldialed";
+    $parameters = array(
+      "callrefid" => $obj->call_uuid,
+      "fromnumber" => $obj->caller_id_number,
+      "tonumber" => $obj->destination_number,
+      "customerstatus" => $custstatus,
+    );
+  break;
+  case "outgoing_call_ended":
+    $url = "https://www.zohoapis.com/crm/v2/phonebridge/callhungup";
+    $parameters = array(
+      "callrefid" => $obj->call_uuid,
+      "fromnumber" => $obj->caller_id_number,
+      "tonumber" => $obj->destination_number,
+      "callstarttime" => prettyDate($obj->answered_at),
+      "direction" => "outbound",
+      "duration" => $obj->duration,
+    );
+  break;
+  case "incoming_call_ended_and_voicemail_left":
+    $url = "https://www.zohoapis.com/crm/v2/phonebridge/voiceurl";
+    $parameters = array(
+      "callrefid" => $obj->call_uuid,
+      "voiceurl" => $obj->voicemail_url,
+    );
+  break;
+}
+
+// Encode in JSON
+$data_string = http_build_query($parameters, '', '&');
+
+$datalogs = "";
+$datalogs .= "\n[". $date . "] Encoded parameters are : ";
+$datalogs .= $data_string;
+$datalogs .= "\n";
+
+$dlogs = fopen('/tmp/zoho_create_activity.txt', 'a+');
+fwrite($dlogs, $datalogs);
+fclose($dlogs);
+
+//Now let's send a request to Zoho
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, count($parameters));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  'Authorization: Zoho-oauthtoken ' . $obj->zoho_oauth_token,
+));
+
+$output = curl_exec($ch);
+$info = curl_getinfo($ch);
+curl_close($ch);
+
+// create an array from the data that is sent back from the API
+$result = json_decode($output, 1);
+
+if (empty($result)) {
+  $success = true;
+  $response['ok'] = true;
+  $response['message'] = "Successfully created activity";
+} else {
+  $logs .= "Failed to create activity.\n";
+  $success = false;
+  $response['ok'] = false;
+  $response['message'] = "Failed to create activity!\nResponse from Zoho: " . $output . "\n";
+}
+
+sendResponseAndExit($response, $logs);
+
+function sendResponseAndExit($response, $logs){
+  header('Content-Type: application/json');
+  echo json_encode($response);
+
+  $fplogs = fopen('/tmp/zoho_create_activity.txt', 'a+');
+  fwrite($fplogs, $logs);
+  fclose($fplogs);
+  exit;
+}
+
+function prettyDate($date){
+  $str = preg_replace('/T/', ' ', $date);
+  $arr = explode(".", $str);
+  $pretty_date = $arr[0];
+  return $pretty_date;
+}
+
+?>
